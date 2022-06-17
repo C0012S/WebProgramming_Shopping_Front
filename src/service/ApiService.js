6de25e6e166c460cdaa59932bf943ab6f3c2def0
C0012S <alwaysh1002@gmail.com>
2022-06-17 14:26:06 +0900

    로컬 스토리지를 이용한 토큰 저장

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,19 @@
 import { API_BASE_URL } from "../app-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+
+    // 로컬 스토리지에서 ACCESS_TOKEN 가져오기
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    if (accessToken && accessToken !== null) {
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -36,6 +45,8 @@ export function signin(userDTO) {
     return call("/auth/signin", "POST", userDTO)
         .then((response) => {
             if (response.token) {
+                // 로컬 스토리지에 토큰 저장
+                localStorage.setItem("ACCESS_TOKEN", response.token);
                 // token이 존재하는 경우 쇼핑몰 페이지 화면으로 리디렉트
                 window.location.href = "/";
             }
